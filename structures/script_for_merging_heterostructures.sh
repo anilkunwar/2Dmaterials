2f4c9d51b3a652679759acf5fc6f0027c6d9e73d
Anil Kunwar <anilkunwar318@yahoo.com>
2025-02-25 19:12:41 +0100

    Add files via upload

---
+++ structures/script_for_merging_heterostructures.sh
@@ -0,0 +1,7 @@
+atomsk ./cif-files/trigonalAl2O3.cif -duplicate 10 10 4 sapphire_substrate.xsf cfg
+atomsk sapphire_substrate.xsf -orthogonal-cell al2o3-ortho.xsf cfg
+atomsk ./cif-files/hexMoS2.cif -duplicate 10 10 1 mos2d.xsf cfg
+atomsk  mos2d.xsf  -orthogonal-cell mos2d-ortho.xsf cfg
+atomsk --merge Z 2 al2o3-ortho.xsf mos2d-ortho.xsf mos2datal2o3-ortho.xsf cfg 
+atomsk mos2datal2o3-ortho.xsf lammps
+
